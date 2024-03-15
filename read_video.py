644fb3d7587672b5ff925b327daf73bd15c12234
Gagan <gagandeep@emovid.com>
2024-03-14 22:20:36 -0600

    video

--- read_video.py
+++ read_video.py
@@ -8,7 +8,7 @@ vid = cv2.VideoCapture("videos/test_video.mp4")
 while True:
     success, img = vid.read()
     if success:
-        # img = cv2.resize(img, (frameWidth, frameHeight))
+        img = cv2.resize(img, (frameWidth, frameHeight))
         cv2.imshow("Video", img)
         if cv2.waitKey(71) & 0xFF == ord('q'):
             break
